a210818322a69c78e1a9d4ac42a131c0133e756b
Dhanraj Singh <sdhanraj300@gmail.com>
2023-11-03 14:04:47 +0530

    added userModel

--- mern-estate/backend/index.js
+++ mern-estate/backend/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import connectToDb from './config/db.js';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+connectToDb();
 
 app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
---
+++ mern-estate/backend/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+const userSchema = mongoose.Schema(
+  {
+    usernname: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//encypting password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
