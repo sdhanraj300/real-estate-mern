eae023cbd6d24e690cea6311b376fa20168df7d3
Dhanraj Singh <sdhanraj300@gmail.com>
2023-11-03 11:19:43 +0530

    created pages and routes structure

--- mern-estate/frontend/src/App.jsx
+++ mern-estate/frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { SignIn, SignUp, Home, Profile, About } from "./pages";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 const App = () => {
   return (
-    <div className='text-3xl text-red-500'>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ mern-estate/frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import About from "./About";
+import Profile from "./Profile";
+import Home from "./Home";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+export { About, Profile, Home, SignIn, SignUp };
