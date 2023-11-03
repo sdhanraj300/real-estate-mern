ee3bca64e3a189bb7fb7b46fdb2467f919489c62
Dhanraj Singh <sdhanraj300@gmail.com>
2023-11-03 11:43:59 +0530

    added the ui of header

--- mern-estate/frontend/src/App.jsx
+++ mern-estate/frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { SignIn, SignUp, Home, Profile, About } from "./pages";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Header from "./components/Header";
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ mern-estate/frontend/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <header className="shadow-md bg-blue-100">
+      <div className="flex justify-between p-3 items-center max-w-6xl mx-auto font-bold">
+        <Link to="/">
+          <h1 className="text-sm font-bold sm:text-2xl flex flex-wrap">
+            <span className="text-slate-500">Realest</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form
+          action=""
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <button className="bg-slate-200 p-3 rounded-lg">
+            <FaSearch />
+          </button>
+        </form>
+        <ul className="flex justify-between gap-4 text-xl">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/signin">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
