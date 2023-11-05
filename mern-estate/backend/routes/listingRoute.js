1b360219cb0491f92ea805eb8f8e4f4b730506ab
Dhanraj Singh <sdhanraj300@gmail.com>
2023-11-06 00:44:39 +0530

    updater header

--- mern-estate/backend/routes/listingRoute.js
+++ mern-estate/backend/routes/listingRoute.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router();
@@ -12,4 +13,5 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 export default router;
--- mern-estate/frontend/src/components/Header.jsx
+++ mern-estate/frontend/src/components/Header.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchTerm);
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) setSearchTerm(searchTermFromUrl);
+  }, [location.search]);
   return (
     <header className="shadow-md bg-blue-100">
       <div className="flex justify-between p-3 items-center max-w-6xl mx-auto font-bold">
@@ -14,15 +29,18 @@ const Header = () => {
           </h1>
         </Link>
         <form
-          action=""
+          action="POST"
+          onSubmit={handleSubmit}
           className="bg-slate-100 p-3 rounded-lg flex items-center"
         >
           <input
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
             type="text"
             placeholder="Search"
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <button className="bg-slate-200 p-3 rounded-lg">
+          <button type="submit" className="bg-slate-200 p-3 rounded-lg">
             <FaSearch />
           </button>
         </form>
